a3a70393c5a6f365619df8f731b9b2b761ac48ad
Ruud Helderman <r.helderman@hccnet.nl>
2025-07-20 23:12:35 +0200

    Chaos game (as fallback).

--- poc/basic.js
+++ poc/basic.js
@@ -4,11 +4,6 @@ const clock = document.getElementById('clock');
 const canvas = document.getElementById('canvas');
 const sandbox = glsl.of(canvas);	// https://github.com/actarian/glsl-canvas/
 
-window.onload = function() {
-	trans();
-	run();
-}
-
 window.setInterval(function() {
 	clock.innerText = (sandbox.timer.current / 1000).toFixed(1);
 }, 100);
@@ -21,20 +16,24 @@ function trans() {
 	s = s.replace(/^\s*/gm, '');
 	s = s.replace(/\s*'.*$/gm, '');
 
-	// Eliminate outer loops - tailored for Eric's fractal
-	s = s.replace(/^For\s+(a)\b.*$/gmi, '$1 = gl_FragCoord.x + .5*u_resolution.x\n{');
-	s = s.replace(/^For\s+(b)\b.*$/gmi, '$1 = gl_FragCoord.y + .5*u_resolution.y\n{');
-
 	// Outcomment unnecessary statements
 	s = s.replace(/^end$|^nomainwin$|^scan$|^wait$|^.*\bProgName\$.*$/gmi, '//$&');
 
 	s = s.replace(/\bAnd\b/gmi, '&&');
+	s = s.replace(/\bNot\b/gmi, '!');
 	s = s.replace(/\bOr\b/gmi, '||');
 	s = s.replace(/\bAbs\b/gmi, 'abs');
-	s = s.replace(/\bCos\b/gmi, 'cos');
-	s = s.replace(/\bSin\b/gmi, 'sin');
+	s = s.replace(/\bAcs\b/gmi, 'acos');
+	s = s.replace(/\bAsn\b/gmi, 'asin');
 	s = s.replace(/\bAtn\b/gmi, 'atan');
-	s = s.replace(/\bInt\b/gmi, 'trunc');
+	s = s.replace(/\bCos\b/gmi, 'w_cos');
+	s = s.replace(/\bExp\b/gmi, 'exp');
+	s = s.replace(/\bInt\b/gmi, 'w_trunc');
+	s = s.replace(/\bLog\b/gmi, 'log');
+	s = s.replace(/\bMax\b/gmi, 'max');
+	s = s.replace(/\bMin\b/gmi, 'min');
+	s = s.replace(/\bSin\b/gmi, 'w_sin');
+	s = s.replace(/\bTan\b/gmi, 'w_tan');
 
 	// Make sure every numeric literal has a decimal point
 	s = s.replace(/\b(?<!\.)(\d+)\b(?!\.)/gm, '$&.');
@@ -51,10 +50,16 @@ function trans() {
 	s = s.replace(/=/gm, '==');
 	s = s.replace(/<assign>/gm, '=');
 
+	// Declare variables
+	const floats = s.match(/^\w+(?=\s*=)/gm);
+	const vec3s = s.match(/^\w+(?=\$\s*=)/gm);
+	if (floats && floats.length > 0) prelude += 'float ' + [...new Set(floats)].join(', ') + ';\n';
+	if (vec3s && vec3s.length > 0) prelude += 'vec3 ' + [...new Set(vec3s)].join(', ') + ';\n';
+
 	// Loop
 	s = s.replace(/^Do$/gmi, 'for (int ii=0; ii<999; ii++) {');
 	s = s.replace(/^(?:Do\s+)?While\b\s*(.*)$/gmi, 'for (int ii=0; ii<999; ii++) {\nif (!bool($1)) break;');
-	s = s.replace(/^For\s+(\w+)\s*=\s*(.*?)\s*\bTo\b\s*(.*)$/gmi, 'for ($1 = $2; $1 <= $3; $1++) {');
+	s = s.replace(/^For\s+(\w+)\s*=+\s*(.*?)\s*\bTo\b\s*(.*)$/gmi, '{\nfloat $1 = w_for($2, $3);');
 	s = s.replace(/^Next\b.*$|^End\s.*$|^Loop$|^Wend$/gmi, '}');
 	s = s.replace(/^Loop\s+Until\b\s*(.*)$/gmi, 'if (bool($1)) break;\n}');
 	s = s.replace(/^Exit\b.*$/gmi, 'break;');
@@ -62,18 +67,12 @@ function trans() {
 	// Add trailing semicolon
 	s = s.replace(/^\w+\s*=.*$/gm, '$&;');
 
-	// Libery BASIC streams
-	//s = s.replace(/^#w\s*"fill\s+black"$/gmi, 'gl_FragColor.rgb = vec3(0);');
-	s = s.replace(/^#w\s*"color\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)$/gmi, 'gl_FragColor.rgb = vec3($1, $2, $3) / 255.;');
+	// Liberty BASIC streams
+	s = s.replace(/^#w\s*"color\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)$/gmi, 'w_color = vec4($1, $2, $3, 255) / 255.;');
+	s = s.replace(/^#w\s*"line\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)\s*;\s*"\s+"\s*;\s*(.*?)$/gmi, 'if (w_line($1, $2, $3, $4)) gl_FragColor = w_color;');
 	s = s.replace(/^.*#w\b.*$/gmi, '//$&');
 
-	// Declare variables
-	const floats = s.match(/^\w+(?=\s*=)/gm);
-	const vec3s = s.match(/^\w+(?=\$\s*=)/gm);
-	if (floats && floats.length > 0) prelude += 'float ' + [...new Set(floats)].join(', ') + ';\n';
-	if (vec3s && vec3s.length > 0) prelude += 'vec3 ' + [...new Set(vec3s)].join(', ') + ';\n';
-
-	shader.value = prelude + 'void main() {\ngl_FragColor = vec4(0,0,0,1);\n' + s + '}\n';
+	shader.value = prelude + s + '\n}\n#endif\n}\n';
 }
 
 function run() {
